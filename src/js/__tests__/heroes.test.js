ef7831ebc9a5064d665f20d2fa5ba6d3ed0177b1
ilyapatis24 <ilyapatis24@mail.ru>
2024-11-11 21:40:29 +0300

    fix code

--- src/js/__tests__/heroes.test.js
+++ src/js/__tests__/heroes.test.js
@@ -1,128 +1,40 @@
-import { Character, Bowerman, Swordsman, Magician, Daemon, Undead, Zombie } from '../heroes.js';
+import Daemon from "../daemon";
+import Magician from "../magician";
 
-test('should attacked', () => {
-    let daemon = new Character('John', 'Daemon', 100)
-    expect(daemon.attacked).toBe(100)
-    expect(daemon.attacked = 2).toBe(2)
-    expect(daemon.attacked).toBe(90)
-    expect(daemon.attacked = 12).toBe(12)
-    expect(daemon.attacked).toBe(0)
-
-    let magician = new Character('John', 'Magician', 100)
-    expect(magician.attacked).toBe(100)
-    expect(magician.attacked = 2).toBe(2)
-    expect(magician.attacked).toBe(90)
-    expect(daemon.attacked = 12).toBe(12)
-    expect(daemon.attacked).toBe(0)
-})
-
-test('should stoned', () => {
-    let daemon = new Character('John', 'Daemon', 100)
-    expect(daemon.stoned).toBe(100)
-    expect(daemon.stoned = 2).toBe(2)
-    expect(daemon.stoned).toBe(85)
-
-    let magician = new Character('John', 'Magician', 100)
-    expect(magician.stoned).toBe(100)
-    expect(magician.stoned = 2).toBe(2)
-    expect(magician.stoned).toBe(85)
-})
-
-test('should be magician', () => {
-    let magician = new Magician('John')
-    expect(magician.attacked).toBe(10)
-    expect(magician.attacked = 1).toBe(1)
-    expect(magician.attacked).toBe(10)
-    expect(magician.stoned).toBe(10)
-    expect(magician.stoned = 1).toBe(1)
-    expect(magician.stoned).toBe(10)
-})
-
-test('should be daemon', () => {
-    let daemon = new Daemon('John')
-    expect(daemon.attacked).toBe(10)
-    expect(daemon.attacked = 1).toBe(1)
-    expect(daemon.attacked).toBe(10)
-    expect(daemon.stoned).toBe(10)
-    expect(daemon.stoned = 1).toBe(1)
-    expect(daemon.stoned).toBe(10)
-})
-
-test('should be error', () => {
-    let daemon = new Character('John', 'Daemon', 100)
-    expect(daemon.attacked).toBe(100)
-    expect(function() {
-        daemon.attacked = 0
-    }).toThrow(new Error('Wrong attacked distance...'));
-    expect(function() {
-        daemon.stoned = 0
-    }).toThrow(new Error('Wrong stoned distance...'));
-})
-
-test('should mistake', () => {
-    expect(function() {
-        new Zombie('')
-    }).toThrow(new Error('Name is too short / long...'));
-    expect(function() {
-        new Character('Monster', '')
-    }).toThrow(new Error('Wrong type of Hero...'));
+test.each([
+	[100, 1, true, 100],
+	[100, 2, true, 85],
+	[100, 1, false, 100],
+	[100, 2, false, 90],
+])('test1', (attack, distance, stoned, expected) => {
+    const magician = new Magician('John', 'Magician');
+    magician.attack = attack;
+    magician.distance = distance;
+    magician.stoned = stoned;
+    expect(magician.attack).toBe(expected);
 });
 
-test('should objects', () => {
-    expect(new Character('Monster', 'Daemon')).toEqual({
-        name: 'Monster',
-        type: 'Daemon',
-        health: 100,
-        level: 1,
-        attack: undefined,
-        defence: undefined
-    });
-    expect(new Bowerman('John')).toEqual({
-        name: 'John',
-        type: 'Bowerman',
-        health: 100,
-        level: 1,
-        attack: 25,
-        defence: 25
-    });
-    expect(new Swordsman('John')).toEqual({
-        name: 'John',
-        type: 'Swordsman',
-        health: 100,
-        level: 1,
-        attack: 40,
-        defence: 10
-    });
-    expect(new Magician('John')).toEqual({
-        name: 'John',
-        type: 'Magician',
-        health: 100,
-        level: 1,
-        attack: 10,
-        defence: 40
-    });
-    expect(new Daemon('John')).toEqual({
-        name: 'John',
-        type: 'Daemon',
-        health: 100,
-        level: 1,
-        attack: 10,
-        defence: 40
-    });
-    expect(new Undead('John')).toEqual({
-        name: 'John',
-        type: 'Undead',
-        health: 100,
-        level: 1,
-        attack: 25,
-        defence: 25
-    });
-    expect(new Zombie('John')).toEqual({
-        name: 'John',
-        type: 'Zombie',
-        health: 100,
-        level: 1,
-        attack: 40,
-        defence: 10
-    });
+    test.each([
+    [100, 1, true, 100],
+    [100, 2, true, 85],
+    [100, 1, false, 100],
+    [100, 2, false, 90],
+])('test2', (attack, distance, stoned, expected) => {
+    const daemon = new Daemon('John', 'Daemon');
+        daemon.attack = attack;
+        daemon.distance = distance;
+        daemon.stoned = stoned;
+    expect(daemon.attack).toBe(expected);
 });
+
+
+    test('Magician stoned', () => {
+    const magician = new Magician('John', 'Magician');
+    magician.stoned = false;
+    expect(magician.stoned).toBeFalsy();
+    });
+    test('Daemon stoned', () => {
+    const daemon = new Daemon('John', 'Daemon');
+    daemon.stoned = false;
+    expect(daemon.stoned).toBeFalsy();
+    });
